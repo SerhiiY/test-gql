d15ddefa4bc383df50d8ba6d4ca3c06ac84aafd3
SerhiiY <dj.ser961@gmail.com>
2020-06-09 21:05:00 +0300

    added build

---
+++ build/precache-manifest.783a7fcad6b75312b746d7b780b2d598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75bd6cc40645f49c4056a33533ef905c",
+    "url": "/test-gql/index.html"
+  },
+  {
+    "revision": "37b89abf4a0ed3fe1629",
+    "url": "/test-gql/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f271d021d151e80d847",
+    "url": "/test-gql/static/css/main.0c4a6972.chunk.css"
+  },
+  {
+    "revision": "37b89abf4a0ed3fe1629",
+    "url": "/test-gql/static/js/2.a682c2d8.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/test-gql/static/js/2.a682c2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f271d021d151e80d847",
+    "url": "/test-gql/static/js/main.a35c7276.chunk.js"
+  },
+  {
+    "revision": "35c1059624ae705afb5c",
+    "url": "/test-gql/static/js/runtime-main.df66b057.js"
+  }
+]);
---
+++ build/service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/test-gql/precache-manifest.783a7fcad6b75312b746d7b780b2d598.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-gql/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
